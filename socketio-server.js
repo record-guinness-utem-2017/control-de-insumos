19691de7a7a66a63f5883787ebd1b5d061ad8885
Kevin Perez <kevindperezm@gmail.com>
2017-03-06 16:33:41 -0600

    mostrar pantalla de pedido registrado
    Esto muestra una pantalla que confirma al usuario que el pedido fue registrado.

--- socketio-server.js
+++ socketio-server.js
@@ -5,8 +5,8 @@ var debug  = require('debug')('socketio-server');
 io.on('connection', function(socket) {
   debug('Cliente conectado');
 
-  socket.on('crear_nuevo_pedido', function(pedido) {
-    debug('Evento "crear_nuevo_pedido" recibido');
+  socket.on('creado_nuevo_pedido', function(pedido) {
+    debug('Evento "creado_nuevo_pedido" recibido');
     debug(pedido);
     socket.broadcast.emit('creado_nuevo_pedido', pedido);
   });
