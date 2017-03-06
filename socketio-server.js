8db7911d4356030f6b12fa9d8b312e927169df29
Kevin Perez <kevindperezm@gmail.com>
2017-03-06 16:22:02 -0600

    instalar debug y usarlo para loguear creación de nuevo pedido

--- socketio-server.js
+++ socketio-server.js
@@ -1,12 +1,16 @@
 var server = require('http').createServer();
 var io     = require('socket.io')(server);
+var debug  = require('debug')('socketio-server');
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
+  debug('Cliente conectado');
+
   socket.on('crear_nuevo_pedido', function(pedido) {
-    console.log('Nuevo pedido creado.');
+    debug('Evento "crear_nuevo_pedido" recibido');
+    debug(pedido);
     socket.broadcast.emit('creado_nuevo_pedido', pedido);
   });
 });
 
 server.listen(2021);
-console.log('Servidor socket.io escuchando en puerto 2021...');
+debug('Servidor socket.io escuchando en puerto 2021...');
