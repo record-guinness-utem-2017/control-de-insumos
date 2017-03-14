4e42a3deb40215ba43ed32a7b56f50921a95db57
Kevin Perez <kevindperezm@gmail.com>
2017-03-14 11:16:41 -0600

    incluir nuevos insumos

--- ajax/insumos/index.php
+++ ajax/insumos/index.php
@@ -3,18 +3,24 @@
 $insumos = [
   [
     'id'       => 1,
+    'nombre'   => 'Atún',
+    'img_url'  => '/assets/img/atun.png',
+    'unidades' => $unidades,
+  ],
+  [
+    'id'       => 2,
     'nombre'   => 'Jitomate',
     'img_url'  => '/assets/img/insumo.png',
     'unidades' => ['kilos', 'cajas'],
   ],
   [
-    'id'       => 2,
+    'id'       => 3,
     'nombre'   => 'Cebolla',
     'img_url'  => '/assets/img/insumo.png',
     'unidades' => ['kilos', 'cajas'],
   ],
   [
-    'id'       => 3,
+    'id'       => 4,
     'nombre'   => 'Cilantro',
     'img_url'  => '/assets/img/insumo.png',
     'unidades' => ['kilos', 'cajas'],
