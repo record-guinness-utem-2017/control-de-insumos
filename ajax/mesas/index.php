ef51a046fb0ed51b375e789b49b01e5cf28b5ddb
Kevin Perez <kevindperezm@gmail.com>
2017-04-08 22:35:42 -0500

    añadir 8 mesas de prueba

--- ajax/mesas/index.php
+++ ajax/mesas/index.php
@@ -14,6 +14,18 @@ $mesas = [
   [
     'id'     => 3,
     'nombre' => 'C',
+  ],[
+    'id'     => 4,
+    'nombre' => 'D',
+  ],[
+    'id'     => 5,
+    'nombre' => 'E',
+  ],[
+    'id'     => 6,
+    'nombre' => 'F',
+  ],[
+    'id'     => 7,
+    'nombre' => 'G',
   ],
 ];
 
