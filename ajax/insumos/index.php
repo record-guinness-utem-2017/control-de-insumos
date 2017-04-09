df9ddc381fe965ded58b245168ec5aba6a461d4e
Kevin Perez <kevindperezm@gmail.com>
2017-04-08 21:40:26 -0500

    cambiar unidad de insumos de kilo a caja
    Con esto los pedidos podrán encargarse en cajas y no en kilos.

--- ajax/insumos/index.php
+++ ajax/insumos/index.php
@@ -11,7 +11,7 @@ if (isset($_GET['disponibles'])) $query .= " WHERE kg_reales > 0";
 $insumos  = get_filas_desde_query($conexion, $query, function(array $insumo) use ($conexion) : array {
   $insumo['id']       = $insumo['id_almacen'];
   $insumo['nombre']   = $insumo['insumo'];
-  $insumo['unidades'] = [['unidad' => 'kilos']];
+  $insumo['unidades'] = [ ['unidad' => 'cajas'] ];
 
   return $insumo;
 });
