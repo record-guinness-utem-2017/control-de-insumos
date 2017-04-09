5255a555dd94fb70af8e7875fe24749712671242
Kevin Perez <kevindperezm@gmail.com>
2017-04-08 21:43:28 -0500

    disminuir cantidad de cajas en lugar de kilos al entregar pedido

--- ajax/pedidos/entregar.php
+++ ajax/pedidos/entregar.php
@@ -9,14 +9,14 @@ $pedido_id = $conexion->escape_string($_POST['id']);
 $conexion->begin_transaction();
 
 $pedido            = get_filas_desde_query($conexion, "SELECT * FROM pedidos WHERE id = $pedido_id")[0];
-$query             = "SELECT kg_reales FROM insumos_config WHERE id_almacen = {$pedido['insumo_id']}";
+$query             = "SELECT cajas_reales FROM insumos_config WHERE id_almacen = {$pedido['insumo_id']}";
 $fila              = get_filas_desde_query($conexion, $query)[0];
-$kilos_disponibles = $fila['kg_reales'];
+$kilos_disponibles = $fila['cajas_reales'];
 
 if ($kilos_disponibles >= $pedido['cantidad']) {
   $sql0 = "UPDATE insumos_config SET 
-             kg_reales     = kg_reales - {$pedido['cantidad']},
-             kg_entregados = kg_entregados + {$pedido['cantidad']}
+             cajas_reales     = cajas_reales - {$pedido['cantidad']},
+             cajas_entregadas = cajas_entregadas + {$pedido['cantidad']}
            WHERE id_almacen = {$pedido['insumo_id']}";
   $conexion->query($sql0) or reportar_error_sql($conexion, $sql0);
 
