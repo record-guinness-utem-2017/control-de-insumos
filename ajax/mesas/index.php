0264627a878e31b687a7ac2bb025ad058ac2463e
Kevin Perez <kevindperezm@gmail.com>
2017-04-13 01:06:25 -0500

    cargar mesas correctas según usuario en selector
    Esto muestra las mesas que el usuario actual supervisa en el selector
    de mesas al crear un nuevo pedido.

--- ajax/mesas/index.php
+++ ajax/mesas/index.php
@@ -2,31 +2,15 @@
 
 require_once __DIR__ . '/../helpers.php';
 
-$mesas = [
-  [
-    'id'     => 1,
-    'nombre' => 'A',
-  ],
-  [
-    'id'     => 2,
-    'nombre' => 'B',
-  ],
-  [
-    'id'     => 3,
-    'nombre' => 'C',
-  ],[
-    'id'     => 4,
-    'nombre' => 'D',
-  ],[
-    'id'     => 5,
-    'nombre' => 'E',
-  ],[
-    'id'     => 6,
-    'nombre' => 'F',
-  ],[
-    'id'     => 7,
-    'nombre' => 'G',
-  ],
-];
+/** @var Mysqli $conexion */
+$conexion = require_once __DIR__ . '/../bd.php';
 
-responder_json_de_objetos($mesas, []);
+$usuario = $conexion->escape_string($_GET['user'] ?? 0);
+$query   = "SELECT * FROM mesas m JOIN mesas_encargados me ON me.mesa_id = m.id WHERE me.encargado_id = $usuario";
+$mesas   = get_filas_desde_query($conexion, $query);
+
+if ($conexion->error) {
+  reportar_error_sql($conexion, $query);
+} else {
+  responder_json_de_objetos($mesas, compact('query'));
+}
