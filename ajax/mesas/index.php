ec61a09aecf748b260cdd131eb0556605a9a7ca8
Kevin Perez <kevindperezm@gmail.com>
2017-04-13 01:18:50 -0500

    permitir que administradores y directores encarguen pedidos para cualquier mesa

--- ajax/mesas/index.php
+++ ajax/mesas/index.php
@@ -5,9 +5,15 @@ require_once __DIR__ . '/../helpers.php';
 /** @var Mysqli $conexion */
 $conexion = require_once __DIR__ . '/../bd.php';
 
-$usuario = $conexion->escape_string($_GET['user'] ?? 0);
-$query   = "SELECT * FROM mesas m JOIN mesas_encargados me ON me.mesa_id = m.id WHERE me.encargado_id = $usuario";
-$mesas   = get_filas_desde_query($conexion, $query);
+$usuario = isset($_GET['user']) ? $conexion->escape_string($_GET['user']) : null;
+
+if ($usuario) {
+  $query = "SELECT m.* FROM mesas m JOIN mesas_encargados me ON me.mesa_id = m.id WHERE me.encargado_id = $usuario";
+} else {
+  $query = 'SELECT * FROM mesas';
+}
+
+$mesas = get_filas_desde_query($conexion, $query);
 
 if ($conexion->error) {
   reportar_error_sql($conexion, $query);
