2c8521cdfd6271a345ee6a725800138d0c435c86
Admin Ceviche <null>
2017-04-14 06:04:01 -0500

    respaldo de base de datos

--- control_de_insumos.sql
+++ control_de_insumos.sql
@@ -7635,7 +7635,7 @@ CREATE TABLE `pedidos` (
   `entregado_en` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=261 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=262 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `pedidos` WRITE;
@@ -7900,6 +7900,7 @@ INSERT INTO `pedidos` VALUES (257,4,2,1,'cajas',2190,'2017-04-14 05:45:09','entr
 INSERT INTO `pedidos` VALUES (258,50,4,1,'cajas',2386,'2017-04-14 05:47:11','enviado',NULL,'2017-04-14 05:54:09');
 INSERT INTO `pedidos` VALUES (259,1,2,1,'cajas',2538,'2017-04-14 05:50:21','entregado','2017-04-14 05:55:38','2017-04-14 05:55:38');
 INSERT INTO `pedidos` VALUES (260,4,2,1,'cajas',2190,'2017-04-14 06:01:39','entregado','2017-04-14 06:02:37','2017-04-14 06:02:37');
+INSERT INTO `pedidos` VALUES (261,2,2,1,'cajas',2538,'2017-04-14 06:03:22','pendiente',NULL,'2017-04-14 06:03:22');
 /*!40000 ALTER TABLE `pedidos` ENABLE KEYS */;
 UNLOCK TABLES;
 DROP TABLE IF EXISTS `personal`;
